51eb8e1c8f158cc0c0a572c3b541c547ff6dfe2e
railgun5 <88987127+railgun5@users.noreply.github.com>
2022-11-10 11:52:53 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,27 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestListExamples {
-  // Write your grading tests here!
+
+    @Test
+    public void testMerge(){
+        
+        List<String> l1 = new ArrayList<String>();
+        List<String> l2 = new ArrayList<String>();
+        l1.add("a");
+        l1.add("b");
+        l1.add("c");
+        l2.add("a");
+        l2.add("e");
+        l2.add("f");
+        System.out.println(l1);
+        System.out.println(l2);
+
+       List<String> list1 = new ArrayList<String>();
+       list1 = (ArrayList<String>)ListExamples.merge(l1,l2);
+       assertArrayEquals(new String[]{"a","a","b","c","e","f"},list1.toArray());
+
+    }
 }
